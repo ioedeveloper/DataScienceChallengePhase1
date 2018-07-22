13b3985eba62d04689388fd974b9ad3d7c97b56c
ioedeveloper <ioedeveloper@DESKTOP-113GA6A>
2018-07-22 17:38:06 +0100

    Bar Chart

--- src/assets/js/chart-data.js
+++ src/assets/js/chart-data.js
@@ -1,24 +1,4 @@
 var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};		
-	var barChartData = {
-		labels : ["January","February","March","April","May","June","July"],
-		datasets : [
-			{
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "rgba(220,220,220,0.8)",
-				highlightFill: "rgba(220,220,220,0.75)",
-				highlightStroke: "rgba(220,220,220,1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-			},
-			{
-				fillColor : "rgba(48, 164, 255, 0.2)",
-				strokeColor : "rgba(48, 164, 255, 0.8)",
-				highlightFill : "rgba(48, 164, 255, 0.75)",
-				highlightStroke : "rgba(48, 164, 255, 1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-			}
-		]
-
-	}
 	
 			
 	var doughnutData = [
