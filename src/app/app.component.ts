ccac4882b57092f8af6be8c8a965c734d7e93350
ioedeveloper <ioedeveloper@DESKTOP-113GA6A>
2018-07-22 10:34:04 +0100

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ExcelService } from './excel.service';
+import {NgForm, NgModel} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,32 @@ import { ExcelService } from './excel.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'app';
+  isLoading = false;
+  msgNo = 0;
+
   constructor(private spreadsheet: ExcelService) {
+  }
+  onSubmit(nickname: string) {
+    this.isLoading = true;
+    this.spreadsheet.fetchData().subscribe((data) => {
+      const values = data['values'];
+      let score = 0;
+      for (const i in values) {
+        if (values[i][3] === nickname) {
+          // tslint:disable-next-line:radix
+          score = parseInt(values[i][6]) + parseInt(values[i][5]) + parseInt(values[i][4]);
+        }
+      }
+      if (score === 0) {
+        this.msgNo = 3;
+      }
+      this.isLoading = false;
+    });
+  }
+
+  reset() {
+    this.msgNo = 0;
   }
 }
--- src/app/excel.service.ts
+++ src/app/excel.service.ts
@@ -5,5 +5,12 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class ExcelService {
+  // tslint:disable-next-line:max-line-length
+  private url = 'https://sheets.googleapis.com/v4/spreadsheets/1lq6AcmAbkOWidxiN_9gb1LdGAczeOKjbU_r3QpArbu8/values/Form%20Responses%201?key=AIzaSyDBKRDMfrBz9L4S0V-AIKDlCWhK7mIEHV0';
+
   constructor(private http: HttpClient) { }
+
+  fetchData() {
+    return this.http.get(this.url);
+  }
 }
